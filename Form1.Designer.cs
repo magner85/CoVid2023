ce091f4a6101909dcb6d1e7d7a75529729fa9f0b
KUSKOV <KUSKOV@DESKTOP-OT4G7R9>
2023-08-24 09:13:19 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CoVid2023
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            menuStrip1 = new MenuStrip();
+            файлToolStripMenuItem = new ToolStripMenuItem();
+            открытьToolStripMenuItem = new ToolStripMenuItem();
+            сохранитьToolStripMenuItem = new ToolStripMenuItem();
+            видеоToolStripMenuItem = new ToolStripMenuItem();
+            пускToolStripMenuItem = new ToolStripMenuItem();
+            паузаToolStripMenuItem = new ToolStripMenuItem();
+            воспроизвестиToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 27);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(776, 411);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem, видеоToolStripMenuItem, паузаToolStripMenuItem, воспроизвестиToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { открытьToolStripMenuItem, сохранитьToolStripMenuItem });
+            файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            файлToolStripMenuItem.Size = new Size(48, 20);
+            файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            открытьToolStripMenuItem.Size = new Size(180, 22);
+            открытьToolStripMenuItem.Text = "Открыть";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            сохранитьToolStripMenuItem.Size = new Size(180, 22);
+            сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // видеоToolStripMenuItem
+            // 
+            видеоToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { пускToolStripMenuItem });
+            видеоToolStripMenuItem.Name = "видеоToolStripMenuItem";
+            видеоToolStripMenuItem.Size = new Size(52, 20);
+            видеоToolStripMenuItem.Text = "Видео";
+            // 
+            // пускToolStripMenuItem
+            // 
+            пускToolStripMenuItem.Name = "пускToolStripMenuItem";
+            пускToolStripMenuItem.Size = new Size(180, 22);
+            пускToolStripMenuItem.Text = "Пуск";
+            // 
+            // паузаToolStripMenuItem
+            // 
+            паузаToolStripMenuItem.Name = "паузаToolStripMenuItem";
+            паузаToolStripMenuItem.Size = new Size(51, 20);
+            паузаToolStripMenuItem.Text = "Пауза";
+            // 
+            // воспроизвестиToolStripMenuItem
+            // 
+            воспроизвестиToolStripMenuItem.Name = "воспроизвестиToolStripMenuItem";
+            воспроизвестиToolStripMenuItem.Size = new Size(102, 20);
+            воспроизвестиToolStripMenuItem.Text = "Воспроизвести";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem файлToolStripMenuItem;
+        private ToolStripMenuItem открытьToolStripMenuItem;
+        private ToolStripMenuItem сохранитьToolStripMenuItem;
+        private ToolStripMenuItem видеоToolStripMenuItem;
+        private ToolStripMenuItem пускToolStripMenuItem;
+        private ToolStripMenuItem паузаToolStripMenuItem;
+        private ToolStripMenuItem воспроизвестиToolStripMenuItem;
+    }
+}
