14f1bd755951fac36b93f2973a55536afeb7f59e
Никита Кусков <magner85@hotmail.com>
2023-09-01 08:09:24 +0300

    yet another Console Video project

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,25 +35,30 @@
             сохранитьToolStripMenuItem = new ToolStripMenuItem();
             видеоToolStripMenuItem = new ToolStripMenuItem();
             пускToolStripMenuItem = new ToolStripMenuItem();
-            паузаToolStripMenuItem = new ToolStripMenuItem();
             воспроизвестиToolStripMenuItem = new ToolStripMenuItem();
+            кадрToolStripMenuItem = new ToolStripMenuItem();
+            инверсияToolStripMenuItem = new ToolStripMenuItem();
+            матрицаToolStripMenuItem = new ToolStripMenuItem();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // richTextBox1
             // 
+            richTextBox1.Font = new Font("Consolas", 1F, FontStyle.Regular, GraphicsUnit.Point);
             richTextBox1.Location = new Point(12, 27);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(776, 411);
+            richTextBox1.Size = new Size(788, 411);
             richTextBox1.TabIndex = 0;
             richTextBox1.Text = "";
             // 
             // menuStrip1
             // 
-            menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem, видеоToolStripMenuItem, паузаToolStripMenuItem, воспроизвестиToolStripMenuItem });
+            menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem, видеоToolStripMenuItem, кадрToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.Size = new Size(939, 24);
             menuStrip1.TabIndex = 1;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -61,24 +66,26 @@
             // 
             файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { открытьToolStripMenuItem, сохранитьToolStripMenuItem });
             файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            файлToolStripMenuItem.Size = new Size(48, 20);
-            файлToolStripMenuItem.Text = "Файл";
+            файлToolStripMenuItem.Size = new Size(95, 20);
+            файлToolStripMenuItem.Text = "Изображение";
             // 
             // открытьToolStripMenuItem
             // 
             открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
             открытьToolStripMenuItem.Size = new Size(180, 22);
             открытьToolStripMenuItem.Text = "Открыть";
+            открытьToolStripMenuItem.Click += открытьToolStripMenuItem_Click;
             // 
             // сохранитьToolStripMenuItem
             // 
             сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
             сохранитьToolStripMenuItem.Size = new Size(180, 22);
             сохранитьToolStripMenuItem.Text = "Сохранить";
+            сохранитьToolStripMenuItem.Click += сохранитьToolStripMenuItem_Click;
             // 
             // видеоToolStripMenuItem
             // 
-            видеоToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { пускToolStripMenuItem });
+            видеоToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { пускToolStripMenuItem, воспроизвестиToolStripMenuItem });
             видеоToolStripMenuItem.Name = "видеоToolStripMenuItem";
             видеоToolStripMenuItem.Size = new Size(52, 20);
             видеоToolStripMenuItem.Text = "Видео";
@@ -87,25 +94,64 @@
             // 
             пускToolStripMenuItem.Name = "пускToolStripMenuItem";
             пускToolStripMenuItem.Size = new Size(180, 22);
-            пускToolStripMenuItem.Text = "Пуск";
-            // 
-            // паузаToolStripMenuItem
-            // 
-            паузаToolStripMenuItem.Name = "паузаToolStripMenuItem";
-            паузаToolStripMenuItem.Size = new Size(51, 20);
-            паузаToolStripMenuItem.Text = "Пауза";
+            пускToolStripMenuItem.Text = "Спарсить";
+            пускToolStripMenuItem.Click += пускToolStripMenuItem_Click;
             // 
             // воспроизвестиToolStripMenuItem
             // 
             воспроизвестиToolStripMenuItem.Name = "воспроизвестиToolStripMenuItem";
-            воспроизвестиToolStripMenuItem.Size = new Size(102, 20);
+            воспроизвестиToolStripMenuItem.Size = new Size(180, 22);
             воспроизвестиToolStripMenuItem.Text = "Воспроизвести";
+            воспроизвестиToolStripMenuItem.Click += воспроизвестиToolStripMenuItem_Click;
+            // 
+            // кадрToolStripMenuItem
+            // 
+            кадрToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { инверсияToolStripMenuItem, матрицаToolStripMenuItem });
+            кадрToolStripMenuItem.Name = "кадрToolStripMenuItem";
+            кадрToolStripMenuItem.Size = new Size(78, 20);
+            кадрToolStripMenuItem.Text = "Фильтры...";
+            // 
+            // инверсияToolStripMenuItem
+            // 
+            инверсияToolStripMenuItem.Name = "инверсияToolStripMenuItem";
+            инверсияToolStripMenuItem.Size = new Size(128, 22);
+            инверсияToolStripMenuItem.Text = "Инверсия";
+            инверсияToolStripMenuItem.Click += инверсияToolStripMenuItem_Click;
+            // 
+            // матрицаToolStripMenuItem
+            // 
+            матрицаToolStripMenuItem.Name = "матрицаToolStripMenuItem";
+            матрицаToolStripMenuItem.Size = new Size(128, 22);
+            матрицаToolStripMenuItem.Text = "Матрица";
+            матрицаToolStripMenuItem.Click += матрицаToolStripMenuItem_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DisplayMember = "Consolas";
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Courier", "Andale Mono", "Monaco", "Profont", "Monofur", "Proggy", "Droid Sans Mono", "Deja Vu Sans Mono", "Consolas", "Inconsolata" });
+            comboBox1.Location = new Point(806, 64);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 2;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(845, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Шрифт";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(939, 450);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
             Controls.Add(richTextBox1);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
@@ -126,7 +172,11 @@
         private ToolStripMenuItem сохранитьToolStripMenuItem;
         private ToolStripMenuItem видеоToolStripMenuItem;
         private ToolStripMenuItem пускToolStripMenuItem;
-        private ToolStripMenuItem паузаToolStripMenuItem;
         private ToolStripMenuItem воспроизвестиToolStripMenuItem;
+        private ToolStripMenuItem кадрToolStripMenuItem;
+        private ToolStripMenuItem инверсияToolStripMenuItem;
+        private ToolStripMenuItem матрицаToolStripMenuItem;
+        private ComboBox comboBox1;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,11 @@
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
 namespace CoVid2023
 {
     public partial class Form1 : Form
@@ -5,6 +13,332 @@ namespace CoVid2023
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string filePath = "";
+        private bool isInverted = false;
+        static bool IsInitialized;
+        private Bitmap src = null;
+        private static StringBuilder output = new StringBuilder();
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.BackColor = System.Drawing.Color.White;
+            richTextBox1.ForeColor = System.Drawing.Color.Black;
+
+            comboBox1.SelectedIndex = 0;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "image files All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    src = new Bitmap(filePath);
+
+                    string outputFrame = processImage(src);
+                    richTextBox1.Text = outputFrame;
+
+                    isInverted = false;
+                }
+            }
+        }
+        private void инверсияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.BackColor = System.Drawing.Color.White;
+            richTextBox1.ForeColor = System.Drawing.Color.Black;
+
+            Bitmap src = new Bitmap(filePath);
+
+            string outputFrame = "";
+
+            if (!isInverted)
+            {
+                outputFrame = processInvertedImage(src);
+
+                isInverted = true;
+            }
+            else
+            {
+                outputFrame = processImage(src);
+
+                isInverted = false;
+            }
+
+            richTextBox1.Text = outputFrame;
+        }
+        private void матрицаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.BackColor = System.Drawing.Color.Black;
+            richTextBox1.ForeColor = System.Drawing.Color.Green;
+        }
+        private string processImage(Bitmap src)
+        {
+            string outputFrame = "";
+            for (int i = 0; i < src.Height; i++)
+            {
+                for (int j = 0; j < src.Width; j++)
+                {
+                    if (src.GetPixel(j, i).GetBrightness() < 0.1)
+                        outputFrame += "...";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.2)
+                        outputFrame += ",,,";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.3)
+                        outputFrame += "___";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.4)
+                        outputFrame += "+++";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.5)
+                        outputFrame += "ooo";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.6)
+                        outputFrame += "XXX";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.7)
+                        outputFrame += "SSS";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.8)
+                        outputFrame += "BBB";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.9)
+                        outputFrame += "%%%";
+                    else
+                        outputFrame += "@@@";
+                }
+                outputFrame += "\n";
+            }
+            return outputFrame;
+        }
+        void draw(object x)
+        {
+            output = new StringBuilder();
+            FileInfo filePath = (FileInfo)(x);
+
+            Bitmap src = new Bitmap(filePath.FullName);
+
+            for (int h = 0; h < src.Height; h++)
+            {
+                for (int w = 0; w < src.Width; w++)
+                {
+                    var pixel = src.GetPixel(w, h);
+                    var brightness = pixel.GetBrightness();
+
+                    if (brightness > 0.5)
+                    {
+                        output.Append("@");
+                    }
+                    else
+                    {
+                        output.Append(" ");
+                    }
+                }
+                output.AppendLine();
+            }
+            try
+            {
+                this.Invoke(new Action(() =>
+                {
+                    try
+                    {
+                        richTextBox1.Text = output.ToString();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Дождитесь окончания!");
+                    }
+                }));
+            }
+            catch
+            {
+                MessageBox.Show("Дождитесь окончания!");
+            }
+        }
+        private string processInvertedImage(Bitmap src)
+        {
+            string outputFrame = "";
+            for (int i = 0; i < src.Height; i++)
+            {
+                for (int j = 0; j < src.Width; j++)
+                {
+                    if (src.GetPixel(j, i).GetBrightness() < 0.1)
+                        outputFrame += "@@@";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.2)
+                        outputFrame += "%%%";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.3)
+                        outputFrame += "BBB";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.4)
+                        outputFrame += "SSS";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.5)
+                        outputFrame += "XXX";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.6)
+                        outputFrame += "ooo";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.7)
+                        outputFrame += "+++";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.8)
+                        outputFrame += "___";
+                    else if (src.GetPixel(j, i).GetBrightness() < 0.9)
+                        outputFrame += ",,,";
+                    else
+                        outputFrame += "...";
+                }
+                outputFrame += "\n";
+            }
+            return outputFrame;
+        }
+        private async void пускToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.BackColor = System.Drawing.Color.White;
+            richTextBox1.ForeColor = System.Drawing.Color.Black;
+
+            Bitmap src = null;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Video files All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                string ffmpegPath = "D:\\ffmpeg\\bin\\ffmpeg.exe";
+                //string ffprobePath = "D:\\ffmpeg\\bin\\ffprobe.exe";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    await Task.Run(() =>
+                    {
+
+                    });
+                }
+            }
+        }
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Word Document (*.doc)|*.doc";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+                SaveToDocx(fileName);
+                MessageBox.Show("Сохранение успешно!");
+            }
+            else
+                MessageBox.Show("ОШИБКА! Не удалось сохранить файл!!!");
+        }
+
+        private void SaveToDocx(string fileName)
+        {
+            using (WordprocessingDocument doc = WordprocessingDocument.Create(fileName, WordprocessingDocumentType.Document))
+            {
+                MainDocumentPart mainPart = doc.AddMainDocumentPart();
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+
+                // Set Margins
+                SectionProperties sectionProps = new SectionProperties();
+                PageMargin pageMargin = new PageMargin() { Top = 720, Right = 720, Bottom = 720, Left = 720, Header = 0, Footer = 0, Gutter = 0 };
+                sectionProps.Append(pageMargin);
+                body.Append(sectionProps);
+
+                Paragraph paragraph = new Paragraph();
+                Run run = new Run();
+                var font = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+                // Set Font Properties
+                RunProperties runProperties = new RunProperties(
+                    new RunFonts() { Ascii = font, HighAnsi = font },
+                    new FontSize() { Val = "2" }); // Set font size here (in half points)
+
+                Text text = new Text(richTextBox1.Text);
+
+                run.Append(runProperties);
+                run.Append(text);
+                paragraph.Append(run);
+
+                // Set Line Spacing
+                ParagraphProperties paragraphProperties = new ParagraphProperties(
+                    new SpacingBetweenLines() { Line = "240", LineRule = LineSpacingRuleValues.Auto });
+
+                paragraph.Append(paragraphProperties);
+
+                body.Append(paragraph);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    {
+                        richTextBox1.Font = new System.Drawing.Font("Courier", richTextBox1.Font.Size);
+                        break;
+                    }
+                case 1:
+                    {
+                        richTextBox1.Font = new System.Drawing.Font("Andale Mono", richTextBox1.Font.Size);
+                        break;
+                    }
+                case 2:
+                    {
+                        richTextBox1.Font = new System.Drawing.Font("Monaco", richTextBox1.Font.Size);
+                        break;
+                    }
+                case 3:
+                    {
+                        richTextBox1.Font = new System.Drawing.Font("Profont", richTextBox1.Font.Size);
+                        break;
+                    }
+                case 4:
+                    {
+                        richTextBox1.Font = new System.Drawing.Font("Monofur", richTextBox1.Font.Size);
+                        break;
+                    }
+                case 5:
+                    {
+                        richTextBox1.Font = new System.Drawing.Font("Proggy", richTextBox1.Font.Size);
+                        break;
+                    }
+                case 6:
+                    {
+                        richTextBox1.Font = new System.Drawing.Font("Droid Sans Mono", richTextBox1.Font.Size);
+                        break;
+                    }
+                case 7:
+                    {
+                        richTextBox1.Font = new System.Drawing.Font("Deja Vu Sans Mono", richTextBox1.Font.Size);
+                        break;
+                    }
+                case 8:
+                    {
+                        richTextBox1.Font = new System.Drawing.Font("Consolas", richTextBox1.Font.Size);
+                        break;
+                    }
+                case 9:
+                    {
+                        richTextBox1.Font = new System.Drawing.Font("Inconsolata", richTextBox1.Font.Size);
+                        break;
+                    }
+            }
+        }
+
+        private async void воспроизвестиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fullPath = "D:\\Desktop\\Видео";
+
+                DirectoryInfo dir = new DirectoryInfo(fullPath);
+                foreach (FileInfo filePath in dir.GetFiles())
+                {
+                    await Task.Run(() =>
+                    {
+                        draw(filePath);
+                    });
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Дождитесь окончания!");
+            }
         }
     }
 }
